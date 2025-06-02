4c751d748f6ca14af1473b79196bdfd7522a87f9
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-06-02 11:15:04 -0300

    defined first structure with dashboard, da-card and request-row

---
+++ src/app/components/da-card/da-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-da-card',
+  imports: [],
+  templateUrl: './da-card.component.html',
+  styleUrl: './da-card.component.scss'
+})
+export class DaCardComponent {
+
+}
---
+++ src/app/components/request-row/request-row.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-row',
+  imports: [],
+  templateUrl: './request-row.component.html',
+  styleUrl: './request-row.component.scss'
+})
+export class RequestRowComponent {
+
+}
